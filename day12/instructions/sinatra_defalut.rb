687a950ac2a80f3692c4e7c4551087484ef037f2
Hyun Ahn <rationality6@gmail.com>
2016-08-05 08:05:02 -0700

    Day 14 started

--- day12/instructions/sinatra_defalut.rb
+++ day12/instructions/sinatra_defalut.rb
@@ -40,6 +40,22 @@ post '/converter' do
     erb(:converter, layout: :layout)
 end
 
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def time(x, y)
+    x * y
+end
+
+def divided(x, y)
+    x / y
+end
+
 get '/calculator' do
     @where5 = true
     erb(:calculator, layout: :layout)
@@ -50,19 +66,18 @@ post '/calculator' do
 
     @add_first = params[:add_first].to_i
     @add_second = params[:add_second].to_i
-    puts add_second
-    # @add_result = @add_first + @add_second
-    #
-    # @sub_first = params[:sub_first].to_i
-    # @sub_second = params[:sub_second].to_i
-    # @sub_result = @sub_first - @sub_second
-    #
-    # @time_first = params[:time_first].to_i
-    # @time_second = params[:time_second].to_i
-    # @time_result = @time_first * @time_second
-    #
-    # @div_first = params[:div_first].to_i
-    # @div_second = params[:div_second].to_i
-    # @div_result = @sub_first / @sub_second
+    @add_result = add(@add_first, @add_second)
+
+    @sub_first = params[:sub_first].to_i
+    @sub_second = params[:sub_second].to_i
+    @sub_result = subtract(@sub_first, @sub_second)
+
+    @time_first = params[:time_first].to_i
+    @time_second = params[:time_second].to_i
+    @time_result = time(@time_first, @time_second)
+
+    @div_first = params[:div_first].to_i
+    @div_second = params[:div_second].to_i
+    @div_result = divided(@div_first, @div_second)
     erb(:calculator, layout: :layout)
 end
