5eaa51c325161310ec4ad63469992803c568d2cb
Hyun Ahn <rationality6@gmail.com>
2016-08-19 11:36:48 -0700

    Authentication

--- day23/amazon/db/schema.rb
+++ day23/amazon/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818023534) do
+ActiveRecord::Schema.define(version: 20160819043004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20160818023534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "paassword_digest"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   add_foreign_key "reviews", "products"
--- day24/summary/class2.js
+++ day24/summary/class2.js
@@ -1,8 +1,19 @@
-class Polygon{
-  constructor(height,width){
-    this.hegith = height;
-    this.width = width;
-  }
-}
+// class Polygon{
+//   constructor(height,width){
+//     this.hegith = height;
+//     this.width = width;
+//   }
+// }
+//
+// var p = new Polygon(3,4);
 
-var p = new Polygon(3,4);
+// class Polygon {
+  // constructor(height, width) {
+  //   this.name = 'Polygon';
+  //   this.height = height;
+  //   this.width = width;
+  // }
+  // sayName() {
+  //   console.log('Hi, I am a ', this.name + '.');
+  // }
+// }
