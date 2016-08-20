941f0e053dba668dc4ed9aaef87bf7c423249e21
Hyun Ahn <rationality6@gmail.com>
2016-08-20 11:58:44 -0700

    Weekend practice.

---
+++ day25/auth_user/app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def user_signed_in?
+    session[:user_id].present?
+  end
+  helper_method :user_signed_in?
+end
